05492046d7cdf0d29ebbf1cd27e824b9c1a6b8f9
deyi.liu <deyi.liu@temu.com>
2025-08-25 17:29:08 +0800

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -9,7 +9,20 @@ const command = args[0];
 enum GitCommand {
 	INIT = 'init',
 	CAT_FILE = 'cat-file',
-	HASH_OBJECT = 'hash-object'
+	HASH_OBJECT = 'hash-object',
+	LS_TREE = 'ls-tree'
+}
+
+enum GitObjectType {
+	BLOB = 'blob',
+	TREE = 'tree'
+}
+
+enum UnixFileMode {
+	REGULAR_FILE = 100644,
+	EXECUTABLE_FILE = 100755,
+	SYMBOLIC_LINK = 120000,
+	DIR = 40000
 }
 
 switch (command) {
@@ -26,19 +39,8 @@ switch (command) {
 		break;
 	case GitCommand.CAT_FILE: {
 		const [_, __, hash] = args;
-		const dir = hash.slice(0, 2);
-		const blob = hash.slice(2);
-		const buffer = fs.readFileSync(path.resolve('.git/objects', dir, blob));
-		// @ts-ignore
-		zlib.unzip(buffer, (err, buf) => {
-			if (err) {
-				console.error('An error occurred:', err);
-				process.exitCode = 1;
-			}
-			const [_, content] = buf.toString().split('\0');
-
-			process.stdout.write(content);
-		});
+		const str = readGitObjects(hash).toString();
+		print(str.split('\0')[1]);
 		break;
 	}
 	case GitCommand.HASH_OBJECT: {
@@ -65,9 +67,61 @@ switch (command) {
 			'base64'
 		);
 
-		process.stdout.write(hash);
+		print(hash);
 		break;
 	}
+	case GitCommand.LS_TREE:
+		const [_, flag, hash] = args;
+		const buf = readGitObjects(hash);
+		let offset = 0;
+		while (offset < buf.length) {
+			// 1. search mode type --> 0x20
+			const modeStart = offset;
+			const modeEnd = buf.indexOf(0x20, modeStart);
+			const mode = buf.toString('utf-8', modeStart, modeEnd);
+
+			// 2. search file/dir name --> 0x00
+			const nameStart = modeEnd + 1;
+			const nameEnd = buf.indexOf(0x00, modeEnd);
+			const name = buf.toString('utf-8', nameStart, nameEnd);
+
+			// 3. search SHA-1 hash
+			const hashStart = nameEnd + 1;
+			const hashEnd = hashStart + 21;
+			const hash = buf.toString('hex', hashStart, hashEnd);
+
+			if (mode === 'tree') {
+				offset = hashStart;
+				continue;
+			}
+
+			if (flag === '--name-only') {
+				print(`${name}\n`);
+			} else {
+				const objectType =
+					+mode === UnixFileMode.DIR
+						? GitObjectType.TREE
+						: GitObjectType.BLOB;
+				print(`${mode} ${objectType} ${hash} ${name}`);
+			}
+
+			offset = hashEnd;
+		}
+
+		break;
 	default:
 		throw new Error(`Unknown command ${command}`);
+}
+
+function readGitObjects(hash: string) {
+	const dir = hash.slice(0, 2);
+	const blob = hash.slice(2);
+	const buffer = fs.readFileSync(path.resolve('.git/objects', dir, blob));
+
+	// @ts-ignore
+	return zlib.unzipSync(buffer);
+}
+
+function print(str: string) {
+	return process.stdout.write(str, 'utf-8');
 }
